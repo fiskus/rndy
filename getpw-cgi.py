3ab77a029151cbedcaf753b3a566319c9e359a8f
Maxim Chervonny <fiskus@chervonny.ru>
2011-10-26 13:36:20 +0400

    move module inside script

--- getpw-cgi.py
+++ getpw-cgi.py
@@ -2,17 +2,26 @@
 # -*- coding: UTF-8 -*-
 
 import cgi
-
-import getpw
+import hashlib
+import base64
 
 form = cgi.FieldStorage()
+
+def pwget(username, domain, masterPassword):
+    password = hashlib.sha1()
+    password.update(username)
+    password.update(domain)
+    password.update(masterPassword)
+    sha1hash = password.hexdigest()
+    encodedHash = base64.b64encode(sha1hash)
+    return encodedHash[:20]
 
 print "Content-type: text/html\n\n"
 masterPassword = form.getvalue("password")
 domain = form.getvalue("domain")
 username = form.getvalue("username")
 
-password = getpw.get(username, domain, masterPassword)
+password = pwget(username, domain, masterPassword)
 
 print "<!DOCTYPE>"
 print "<html lang=\"en\">"
