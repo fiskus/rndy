2adba3a605b7910496d880a02fca9e30e84198c2
Maxim Chervonny <fiskus@chervonny.ru>
2012-05-28 01:45:13 +0400

    fix line endings

--- getpw-cgi.py
+++ getpw-cgi.py
@@ -28,11 +28,11 @@ print("<html lang=\"en\">")
 print("<meta charset=\"UTF-8\">")
 print("<meta http-equiv=\"refresh\" content=\"15; url=/\">")
 print("<meta name=\"viewport\" content=\"initial-scale=1.0, user-scalable=no\">")
-print("<style> \)
-	P {text-align: center;} .domain {margin-top: 100px; color: #CCC;} \)
-	.hash INPUT {font-size: 3em; color: #EFEFEF; border: 0; text-align: center;} \)
-	::-moz-selection { background:#EFEFEF; color:#FFF; } \)
-	::selection { background:#EFEFEF; color:#FFF; } \)
+print("<style> \
+	P {text-align: center;} .domain {margin-top: 100px; color: #CCC;} \
+	.hash INPUT {font-size: 3em; color: #EFEFEF; border: 0; text-align: center;} \
+	::-moz-selection { background:#EFEFEF; color:#FFF; } \
+	::selection { background:#EFEFEF; color:#FFF; } \
 	</style>")
 print("<body>")
 print("<p class=domain>" + domain + "</p>")
