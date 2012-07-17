bec0a5a18c51f5ffa13a0706dde4214b77d95809
Maxim Chervonny <fiskus@chervonny.ru>
2012-07-17 18:08:27 +0400

    add ability to set password in cli argument

--- psw.sh
+++ psw.sh
@@ -3,7 +3,7 @@
 USERNAME=fiskus
 COUNT=20
 
-while getopts ":d:u:c:" opt; do
+while getopts ":d:u:c:p:" opt; do
     case $opt in
         d)
             DOMAIN=$OPTARG
@@ -14,6 +14,9 @@ while getopts ":d:u:c:" opt; do
         c)
             COUNT=$OPTARG
             ;;
+        p)
+            MASTERPASSWORD=$OPTARG
+            ;;
         \?)
             echo "Usage:"
             echo "psw -d google.com -u sergey -c 20"
@@ -22,14 +25,16 @@ while getopts ":d:u:c:" opt; do
 done
 
 
-if [[ ! -a $HOME/.masterpassword ]]; then
-    echo -n 'Type master-password: '
-    read -s MASTERPASSWORD
-    echo -n $MASTERPASSWORD > $HOME/.masterpassword
-    echo '' #new line for spliting prompt and password
-else
-    MASTERPASSWORD=`cat $HOME/.masterpassword`
-    chmod 0700 $HOME/.masterpassword
+if [[ ! $MASTERPASSWORD ]]; then
+    if [[ ! -a $HOME/.masterpassword ]]; then
+        echo -n 'Type master-password: '
+        read -s MASTERPASSWORD
+        echo -n $MASTERPASSWORD > $HOME/.masterpassword
+        echo '' #new line for spliting prompt and password
+    else
+        MASTERPASSWORD=`cat $HOME/.masterpassword`
+        chmod 0700 $HOME/.masterpassword
+    fi
 fi
 
 PASSWORD=`echo -n $USERNAME$DOMAIN$MASTERPASSWORD | sha1sum | base64 | cut -c 1-$COUNT`
