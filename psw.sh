f1687183616df441d51115972c67152d8d9cc3d3
Maxim Chervonny <fiskus@chervonny.ru>
2012-07-17 17:10:17 +0400

    keep masterpassword until seession ended

--- psw.sh
+++ psw.sh
@@ -22,7 +22,14 @@ while getopts ":d:u:c:" opt; do
 done
 
 echo -n 'Type master-password: '
-read -s MASTERPASSWORD
+
+if [ ! -a $HOME/.masterpassword ]; then
+    read -s MASTERPASSWORD
+    echo -n $MASTERPASSWORD > $HOME/.masterpassword
+else
+    MASTERPASSWORD=`cat $HOME/.masterpassword`
+    chmod 0700 $HOME/.masterpassword
+fi
 
 echo $COUNT
 echo -n $USERNAME$DOMAIN$MASTERPASSWORD | sha1sum | base64 | cut -c 1-$COUNT
---
+++ zlogout
@@ -0,0 +1,4 @@
+if [ `ps ax | grep -v grep | grep zsh | wc -l` -eq 1 ];
+then
+	rm $HOME/.masterpassword
+fi
