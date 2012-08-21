e602c12f341ab64128439e6ca0265ca562f586b5
Maxim Chervonny <fiskus@chervonny.ru>
2012-08-21 15:40:43 +0400

    some refactoring

--- getpw-cgi.py
+++ getpw-cgi.py
@@ -1,31 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
+import subprocess
 import cgi
-from subprocess import call
 
 form = cgi.FieldStorage()
 
 print("Content-type: text/html\n\n")
-masterPassword = form.getvalue("password")
-domain = form.getvalue("domain")
-username = form.getvalue("username")
-count = form.getvalue("count")
 
-password = call(username, domain, masterPassword)
+masterPassword = form.getvalue('password')
+domain = form.getvalue('domain')
+username = form.getvalue('username')
+count = form.getvalue('count')
+
+password = subprocess.check_output(['./psw.sh', '-u', username, '-d', domain, '-p', masterPassword, '-c', count])
 
 print("<!DOCTYPE>")
 print("<html lang=\"en\">")
 print("<meta charset=\"UTF-8\">")
 print("<meta name=\"viewport\" content=\"initial-scale=1.0, user-scalable=no\">")
-print("<style> \
-	P {text-align: center;} .domain {margin-top: 100px; color: #CCC;} \
-	.hash INPUT {font-size: 3em; color: #EFEFEF; border: 0; text-align: center;} \
-	::-moz-selection { background:#EFEFEF; color:#FFF; } \
-	::selection { background:#EFEFEF; color:#FFF; } \
-	</style>")
+print("<link rel=\"stylesheet\" href=\"/style.css\" />")
 print("<body>")
 print("<p class=domain>" + domain + "</p>")
-print("<p class=hash><input value=" + password + " autofocus/></p>")
+print("<p class=hash><input value=" + password.decode('ascii') + " autofocus/></p>")
 print("</body>")
 print("</html>")
--- psw.sh
+++ psw.sh
@@ -3,23 +3,31 @@
 USERNAME=fiskus
 COUNT=20
 
-while getopts ":d:u:c:p:" opt; do
+while getopts ":d:u:c:p:o" opt; do
     case $opt in
+        #set domain name
         d)
             DOMAIN=$OPTARG
             ;;
+        #set username
         u)
             USERNAME=$OPTARG
             ;;
+        #set symbols count
         c)
             COUNT=$OPTARG
             ;;
+        #set symbols' count
         p)
             MASTERPASSWORD=$OPTARG
             ;;
+        #raw output or to xclipboard
+        o)
+            OUTPUT='RAW'
+            ;;
         \?)
             echo "Usage:"
-            echo "psw -d google.com -u sergey -c 20"
+            echo "psw -d google.com -u sergey.brin -c 20"
             ;;
     esac
 done
@@ -39,6 +47,8 @@ fi
 
 PASSWORD=`echo -n $USERNAME$DOMAIN$MASTERPASSWORD | sha1sum | base64 | cut -c 1-$COUNT`
 
-echo -n $PASSWORD | xclip
-
-echo $PASSWORD
+if [[ $OUTPUT ]]; then
+    echo $PASSWORD
+else
+    echo -n $PASSWORD | xclip
+fi
