b92396f2755c613c79201df8b57092c5f38337cf
Maxim Chervonny <fiskus@chervonny.ru>
2012-11-25 02:40:49 +0400

    refs #2 store password in storeSession

--- js/script.js
+++ js/script.js
@@ -10,31 +10,47 @@ PSW.prototype.settings = {
     selectors: {
         form: 'form',
         ajax: '#ajax',
-        output: '.output'
+        output: '.output',
+        password: '#password',
+        passwordReset: '.password-reset'
     }
-}
+};
+
+PSW.prototype.init = function() {
+    this.form = $(this.settings.selectors.form);
+    this.form.on('submit', $.proxy(this._onSubmit, this));
+
+    this.passwordInput = $(this.settings.selectors.password)
+    if (this.isPaswordStored()) {
+        this.hidePasswordInput();
+    }
+};
 
 PSW.prototype.pastePassword = function(text) {
     $(this.settings.selectors.output).html(text);
-}
+};
 
 PSW.prototype._onSubmit = function(event) {
     if (this.isAjax()) {
         event.preventDefault();
         this.serialize();
         this.requestPassword();
+        if (this.isPaswordStored()) {
+            this.hidePasswordInput();
+        }
     } else {
         return;
     }
-}
+};
 
 PSW.prototype.isAjax = function() {
     return $(this.settings.selectors.ajax).prop('checked');
-}
+};
 
 PSW.prototype.serialize = function() {
+    this.storePassword();
     this.data = this.form.serialize();
-}
+};
 
 PSW.prototype.requestPassword = function() {
     var url = this.form.attr('action');
@@ -48,22 +64,44 @@ PSW.prototype.requestPassword = function() {
         error: this._onError,
         dataType: 'text'
     });
-}
+};
 
 PSW.prototype._onSuccess = function(data) {
     this.pastePassword(data);
-}
+};
 
 PSW.prototype._onError = function(data) {
     this.pastePassword('ERROR: ' + data);
+};
+
+PSW.prototype.storePassword = function(data) {
+    sessionStorage.setItem('masterpassword', this.passwordInput.val());
+};
+
+PSW.prototype.isPaswordStored = function() {
+    var password = sessionStorage.getItem('masterpassword');
+
+    if (password) {
+        this.password = password;
+        return true;
+    } else {
+        return false;
+    }
+};
+
+PSW.prototype.hidePasswordInput = function() {
+    this.passwordInput.val(this.password);
+    this.passwordInput.hide();
+    this.passwordReset = $(this.settings.selectors.passwordReset);
+    this.passwordReset.addClass('active');
+    this.passwordReset.on('click', $.proxy(this.showPasswordInput, this));
 }
 
-//PSW.prototype.isPaswordStored = function() {
-//}
-
-PSW.prototype.init = function() {
-    this.form = $(this.settings.selectors.form);
-    this.form.on('submit', $.proxy(this._onSubmit, this));
-
-    //this.isPaswordStored();
+PSW.prototype.showPasswordInput = function() {
+    this.passwordInput
+        .val('')
+        .show()
+        .focus();
+    this.passwordReset.removeClass('active');
+    this.passwordReset.off('click');
 }
